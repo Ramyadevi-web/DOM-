22ab2f50b2c936fed5e75e272ec74a3a895e95e8
Ramyadevi-web <ramyadevim7@gmail.com>
2023-09-30 12:41:01 +0530

    Getting element using DOM in console

---
+++ script.js
@@ -0,0 +1,12 @@
+function collectDetails(){
+var first = document.getElementById("first").value;
+var last = document.getElementById("last").value;
+var middle = document.getElementById("middle").value;
+var email = document.getElementById("email").value;
+var pincode = document.getElementById("pincode").value;
+console.log(first);
+console.log(middle);
+console.log(last);
+console.log(email);
+console.log(pincode);
+}
